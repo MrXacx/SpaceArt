6abcde50524d0d2324deb61a6011173178952137
MrXacx <misterxacx@gmail.com>
2023-07-17 17:46:05 -0300

    corrige index

--- api/src/index.php
+++ api/src/index.php
@@ -1,9 +1,12 @@
 <?php
 
-declare(strict_types = 1);
+header('Content-type: application/json; charset="utf-8"');
 
 foreach(parse_ini_file(__DIR__.'/config/.ini', true)['DEVELOPMENT'] as $key => $value){
     $_ENV[$key] = $value;
 }
+
+
+echo json_encode([true]);
 
 ?>
