4950d7fe4282686e51c3e670789419a795d5d407
MrXacx <misterxacx@gmail.com>
2023-09-13 23:43:20 -0300

    refactor: simplifica setup.php

--- api/config/setup.php
+++ api/config/setup.php
@@ -1,19 +1,12 @@
 <?php
 
-require_once '../vendor/autoload.php'; // Carrega dependências
+require_once __DIR__.'/../vendor/autoload.php'; // Carrega dependências
 
 Locale::setDefault('pt-BR'); // Define charset
 
-/**
- * Obtém as variáveis de ambiente para o banco de dados
- * @param bool $production true caso a execução seja para produção, false caso seja para desenvolvimento
- */
-function getDatabaseSettings(bool $production): array{
-    return parse_ini_file('setup.ini', true)[ $production ? 'DATABASE_PROUCTION' : 'DATABASE_DEVELOPMENT'];  
-}
+$_ENV = array_merge($_ENV, parse_ini_file('setup.ini', true));
 
-$_ENV = array_merge($_ENV, getDatabaseSettings(false));
-
+$_ENV = array_merge($_ENV, $_ENV['DATABASE_DEVELOPMENT']) ;
 
 
 ?>
