c2d2fa7196e7e1361ff46ed6f2b7506a3f9d24f1
MrXacx <misterxacx@gmail.com>
2023-09-16 17:14:30 -0300

    feat: insere log para exceções runtime inexperadas

--- api/public_html/src/index.php
+++ api/public_html/src/index.php
@@ -14,32 +14,37 @@ Server::$logger->build(); // Cria log para o dia
 // Armazena endereço do client
 Server::$logger->push('requisição feita por ' . Server::getClientIP(), Level::Info);
 
-$response = new Symfony\Component\HttpFoundation\Response; // Inicia objeto de resposta
 $response->headers->set('Content-Type', 'application/json'); // Define o formato da resposta
+$response = new Symfony\Component\HttpFoundation\Response; // Inicia objeto de resposta
 
-Controller::$cache = new Cache( // Inicializa manipulação de cache
-    str_replace('/','@', Server::getURI())
+Controller::$cache = new Cache(
+    // Inicializa manipulação de cache
+    str_replace('/', '@', Server::getURI())
 );
 
+try {
+    if (!Controller::$cache->isUsable()) { // Executa se o cache não existir ou estiver expirado
 
-if(!Controller::$cache->isUsable()){ // Executa se o cache não existir ou estiver expirado
+        Server::$logger->push('rotina de consulta ao banco de dados remoto inicializada', Level::Notice); // Informa consulta ao banco de dados
+        $routes = new App\RoutesBuilder;
+        $routes->fetchResponse($response, $routes->dispatch()); // Obtém a resposta da rota
 
-    Server::$logger->push('rotina de consulta ao banco de dados remoto inicializada', Level::Notice); // Informa consulta ao banco de dados
-    $routes = new App\RoutesBuilder; 
-    $routes->fetchResponse($response, $routes->dispatch()); // Obtém a resposta da rota
-
-} else {
-    Server::$logger->push('rotina de consulta ao cache inicializada', Level::Notice);
-    $response->setContent(
-        Controller::$cache->getContent()
-    );
+    } else {
+        Server::$logger->push('rotina de consulta ao cache inicializada', Level::Notice);
+        $response->setContent(
+            Controller::$cache->getContent()
+        );
+    }
+    
+} catch (Exception $ex) {
+    Server::$logger->push('exceção lançada: ' . $ex->getMessage(), Level::Critical); 
 }
 
 $response->send(); // Exibe resposta
 
 $time = microtime(true) - $startTime;
 
-Server::$logger->push('resposta enviada para '. Server::getClientIP(), Level::Info);
+Server::$logger->push('resposta enviada para ' . Server::getClientIP(), Level::Info);
 Server::$logger->push('tempo de resposta: ' . $time . 'ms', Level::Debug);
 
 ?>
