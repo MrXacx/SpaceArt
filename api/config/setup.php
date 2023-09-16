e482ee84eb0388023a2a78d975221166900217af
MrXacx <misterxacx@gmail.com>
2023-09-16 17:18:27 -0300

    fix: habilita configuração para desenvolvimento

--- api/config/setup.php
+++ api/config/setup.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 0); // Deixa de exibir erros 
-header('Acess-Control-Allow-Origin: *'); // Libera o acesso à API
+ini_set('display_errors', 1); // Deixa de exibir erros 
+header('Access-Control-Allow-Origin: *'); // Libera o acesso à API
 
 require_once __DIR__.'/../vendor/autoload.php'; // Carrega dependências
 
