dd240923e6986d48e1d7e208259a7516cd452b05
MrXacx <misterxacx@gmail.com>
2023-10-10 23:23:03 -0300

    feat: adiciona método estático throw

--- src/errors/HTTPRequestError.jsx
+++ src/errors/HTTPRequestError.jsx
@@ -2,4 +2,8 @@ export class HTTPRequestError extends Error {
   constructor(message) {
     this.message = message;
   }
+
+  static throw(message = ''){
+    throw new HTTPRequestError(message);
+  }
 }
--- src/errors/RegExError.jsx
+++ src/errors/RegExError.jsx
@@ -2,4 +2,8 @@ export class RegExError extends Error {
   constructor(message) {
     this.message = message;
   }
+
+  static throw(message = ''){
+    throw new RegExError(message);
+  }
 }
