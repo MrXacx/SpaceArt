67f89c530775fefc8f7931b9ca6c9a7dd741299a
Cloviski <claudioribeiro.j@hotmail.com>
2023-10-12 14:45:14 -0300

    Add feed page

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,6 +1,13 @@
 import Header from "../../components/header/header";
-import { FeedContainer, PostContainer, ProfileContainer } from "./feedStyles";
+import {
+  FeedContainer,
+  PostContainer,
+  ProfileContainer,
+  ProfilePostImage,
+  TextContentContainer,
+} from "./feedStyles";
 import ProfilePicture from "../../assets/profile_user.png";
+import PlaceholderImage from "../../assets/placeholder_image.png";
 
 function Feed() {
   return (
@@ -10,9 +17,26 @@ function Feed() {
         <PostContainer>
           <ProfileContainer>
             <img src={ProfilePicture} alt="foto de perfil" />
-            <span>O brabo</span>
-            <span>7m</span>
+            <div>
+              <span>John Aaron</span>
+              <span>7m</span>
+            </div>
           </ProfileContainer>
+          <TextContentContainer>
+            <p>
+              I spent 24 hours on 1 Coding Problem. Rotate Array was the first
+              problem I tried on LeetCode. I spent the entire day trying to
+              solve it but couldn't. I was extremely frustrated and went to the
+              discussion section to read the most upvoted solution. Another 2
+              hours passed and I could not even figure out why the solution
+              works. At that point, my frustration had transformed into
+              disappointment. I somehow managed to put myself to sleep that
+              night. Next morning, I went to a friend and asked him to solve the
+              exact same problem. And that’s when I learnt something really
+              interesting about human behavior.
+            </p>
+          </TextContentContainer>
+          <ProfilePostImage alt="imagem" src={PlaceholderImage} />
         </PostContainer>
       </FeedContainer>
     </>
--- src/pages/feed/feedStyles.ts
+++ src/pages/feed/feedStyles.ts
@@ -4,21 +4,53 @@ export const FeedContainer = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
+  row-gap: 35px;
   box-sizing: border-box;
-  background-color: #f0f0f0;
+  padding-top: 15vh;
 `;
 
 export const PostContainer = styled.div`
   display: flex;
   flex-direction: column;
-  border-radius: 10px;
-  padding: 15px;
+  border-radius: 20px;
+  padding: 20px;
   width: 500px;
   height: 500px;
-  margin: auto auto;
+  margin: 0 auto;
+  background-color: #fff;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2)
 `;
 
 export const ProfileContainer = styled.div`
   display: flex;
   align-items: center;
+
+  img {
+    max-width: 45px;
+    padding-right: 10px;
+  }
+
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    span {
+      font-size: 1rem;
+
+      &:first-of-type {
+        font-weight: 600;
+      }
+    }
+  }
+`;
+
+export const TextContentContainer = styled.div`
+  p {
+    font-size: 1rem;
+  }
+`;
+
+export const ProfilePostImage = styled.img`
+
 `;
