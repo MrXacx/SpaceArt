2a1ec45a8a0037de392b5f896de17b4bcdc7af9b
MrXacx <misterxacx@gmail.com>
2023-10-14 21:06:58 -0300

    feat: trata resposte de Report.jsx

--- src/models/Report.jsx
+++ src/models/Report.jsx
@@ -1,5 +1,9 @@
 import { WebServiceClient } from "../services/WebServiceClient";
 
+const api = WebServiceClient.request;
+const HTTPRequestError = WebServiceClient.error.HTTPRequestError;
+const status = WebServiceClient.status;
+
 export class Report {
     id; reporter; reported; reason;
     url = 'https://service-spaceart.000webhostapp.com/user/report';
@@ -15,19 +19,30 @@ export class Report {
     }
 
     async create() {
-        let response = WebServiceClient.request.post(
-            this.url,
+        api.post(this.url,
             {
                 reporter: this.reporter,
                 reported: this.reported,
                 reason: this.reason
+            }).then(response => {
+                if (response.status !== status.OK) {
+                    HTTPRequestError.throw(response.statusText);
+                }
             });
     }
 
     async getList(offset = 0, limit = 10) {
-        let response = WebServiceClient.request.get(
+        let response = await api.get(
             `${this.url}?offset=${offset}&limit=${limit}&reporter=${this.reporter}`
         );
+
+        if (response.status !== status.OK) {
+            HTTPRequestError.throw(response.statusText);
+        }
+
+        return response.data.map(
+            report => new Report(report.reporter).factory(report)
+        ); // Instancia todos as denúncias obtidas
     }
 
 }
