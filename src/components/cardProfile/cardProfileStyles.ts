fbd90d22bd9107474e3ed4c7670017286dc188db
Cloviski <claudioribeiro.j@hotmail.com>
2023-10-19 19:08:41 -0300

    Adding new components

--- src/components/cardProfile/cardProfileStyles.ts
+++ src/components/cardProfile/cardProfileStyles.ts
@@ -1,55 +1,5 @@
 import styled from "styled-components";
 
-export const SearchArtistContainer = styled.div`
-  width: 100%;
-  display: flex;
-  background-color: #f5f5f5;
-  color: #fa8b08;
-  justify-content: center;
-  flex-direction: column;
-  padding: 25px 0;
-
-  h2 {
-    text-align: center;
-  }
-`;
-
-export const CategoryContainer = styled.div`
-  width: 70%;
-  display: grid;
-  margin: 0 auto;
-  background-color: black;
-  grid-template-columns: repeat(4, 1fr);
-  flex-direction: column;
-  padding: 30px;
-  column-gap: 60px;
-  border-radius: 5px;
-`;
-
-export const CategoryInput = styled.select`
-  height: 50px;
-  border: none;
-  border-radius: 15px;
-`;
-
-export const CategoryButton = styled.button`
-  width: 100%;
-  height: 50px;
-  font-weight: 600;
-  background-color: #fa8b08;
-  border: none;
-  color: #fff;
-`;
-
-export const CardProfileContainer = styled.div`
-  display: grid;
-  margin: 0 auto;
-  width: 73%;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 2rem;
-  padding: 50px 0;
-`;
-
 export const CardProfileContent = styled.div`
   width: 300px;
   border-radius: 20% 20px 20px 20px;
--- src/components/searchArtist/searchArtist.tsx
+++ src/components/searchArtist/searchArtist.tsx
@@ -1,62 +1,38 @@
 import {
   CardProfileContainer,
-  CardProfileContent,
-  CardProfileDetail,
   CategoryButton,
   CategoryContainer,
   CategoryInput,
   SearchArtistContainer,
-  UserCardDetailContainer,
 } from "./searchArtistStyles";
-import MarcoImage from "../../assets/marco_image.png";
-import LocalIcon from "../../assets/local.svg";
-import WageIcon from "../../assets/wage.svg";
+import CardProfile from "../cardProfile/cardProfile";
 
 function SearchArtist() {
   return (
-    <>
-      <SearchArtistContainer>
-        <h2>BUSQUE ARTISTAS DE SUA CIDADE</h2>
-        <CategoryContainer>
-          <CategoryInput>
-            <option disabled selected>
-              SELECIONE SEU ESTADO
-            </option>
-          </CategoryInput>
-          <CategoryInput>
-            <option disabled selected>
-              SELECIONE SUA CIDADE
-            </option>
-          </CategoryInput>
-          <CategoryInput>
-            <option disabled selected>
-              ESCOLHA TIPO DE ARTE
-            </option>
-          </CategoryInput>
-          <CategoryButton>PESQUISAR</CategoryButton>
-        </CategoryContainer>
-        <CardProfileContainer>
-          <CardProfileContent>
-            <img alt="profile image" src={MarcoImage} />
-            <CardProfileDetail>
-              <div>
-                <span>Marco Antônio</span>
-                <span>Música</span>
-              </div>
-              <UserCardDetailContainer>
-                <img alt="local icon" src={LocalIcon} />
-                <span>Salvador, BA</span>
-              </UserCardDetailContainer>
-              <UserCardDetailContainer>
-                <img alt="wage icon" src={WageIcon} />
-                <span>R$256,00</span>
-              </UserCardDetailContainer>
-            </CardProfileDetail>
-          </CardProfileContent>
-          
-        </CardProfileContainer>
-      </SearchArtistContainer>
-    </>
+    <SearchArtistContainer>
+      <h2>BUSQUE ARTISTAS DE SUA CIDADE</h2>
+      <CategoryContainer>
+        <CategoryInput>
+          <option disabled selected>
+            SELECIONE SEU ESTADO
+          </option>
+        </CategoryInput>
+        <CategoryInput>
+          <option disabled selected>
+            SELECIONE SUA CIDADE
+          </option>
+        </CategoryInput>
+        <CategoryInput>
+          <option disabled selected>
+            ESCOLHA TIPO DE ARTE
+          </option>
+        </CategoryInput>
+        <CategoryButton>PESQUISAR</CategoryButton>
+      </CategoryContainer>
+      <CardProfileContainer>
+        <CardProfile />
+      </CardProfileContainer>
+    </SearchArtistContainer>
   );
 }
 
