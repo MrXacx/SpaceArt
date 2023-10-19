5de319c6cb18eec16a1b2fb943dc4a12e4d1c85b
MrXacx <misterxacx@gmail.com>
2023-10-19 16:36:08 -0300

    chore: corrige identação

--- src/models/Report.jsx
+++ src/models/Report.jsx
@@ -5,56 +5,59 @@ const HTTPRequestError = WebServiceClient.error.HTTPRequestError;
 const status = WebServiceClient.status;
 
 export class Report {
-    id; reporter; reported; reason;
-    url = 'https://service-spaceart.000webhostapp.com/user/report';
+  id;
+  reporter;
+  reported;
+  reason;
+  url = "https://service-spaceart.000webhostapp.com/user/report";
 
-    constructor(reporter) {
-        this.reporter = reporter;
-    }
+  constructor(reporter) {
+    this.reporter = reporter;
+  }
 
-    /**
-     * Preenche todos os atributos
-     * @param object
-     */
-    factory(report) {
-        this.id = report.id;
-        this.reported = report.reported;
-        this.reason = report.reason;
-    }
+  /**
+   * Preenche todos os atributos
+   * @param object
+   */
+  factory(report) {
+    this.id = report.id;
+    this.reported = report.reported;
+    this.reason = report.reason;
+  }
 
-    /**
-     * Cria nova denúncia na API
-     */
-    async create() {
-        api.post(this.url,
-            {
-                reporter: this.reporter,
-                reported: this.reported,
-                reason: this.reason
-            }).then(response => {
-                if (response.status !== status.OK) {
-                    HTTPRequestError.throw(response.statusText);
-                }
-            });
-    }
-
-    /**
-     * Obtém lista de denúncia s de usuário
-     * @param int posição de início da leitura
-     * @param int limite de itens
-     */
-    async getList(offset = 0, limit = 10) {
-        let response = await api.get(
-            `${this.url}?offset=${offset}&limit=${limit}&reporter=${this.reporter}`
-        );
-
+  /**
+   * Cria nova denúncia na API
+   */
+  async create() {
+    api
+      .post(this.url, {
+        reporter: this.reporter,
+        reported: this.reported,
+        reason: this.reason,
+      })
+      .then((response) => {
         if (response.status !== status.OK) {
-            HTTPRequestError.throw(response.statusText);
+          HTTPRequestError.throw(response.statusText);
         }
+      });
+  }
 
-        return response.data.map(
-            report => new Report(report.reporter).factory(report)
-        ); // Instancia todos as denúncias obtidas
+  /**
+   * Obtém lista de denúncia s de usuário
+   * @param int posição de início da leitura
+   * @param int limite de itens
+   */
+  async getList(offset = 0, limit = 10) {
+    let response = await api.get(
+      `${this.url}?offset=${offset}&limit=${limit}&reporter=${this.reporter}`
+    );
+
+    if (response.status !== status.OK) {
+      HTTPRequestError.throw(response.statusText);
     }
 
+    return response.data.map((report) =>
+      new Report(report.reporter).factory(report)
+    ); // Instancia todos as denúncias obtidas
+  }
 }
--- src/models/User.jsx
+++ src/models/User.jsx
@@ -8,276 +8,274 @@ const status = WebServiceClient.status;
  * Classe de consulta de dados de usuários
  */
 class User {
-    id; index; token; type; email; password; phone; name; image; // Todos os atributos úteis retornados
-    location = {}; // Objeto para dados de localização
+  id;
+  index;
+  token;
+  type;
+  email;
+  password;
+  phone;
+  name;
+  image; // Todos os atributos úteis retornados
+  location = {}; // Objeto para dados de localização
 
-    url = 'https://service-spaceart.000webhostapp.com/user'; // URL base para consulta
+  url = "https://service-spaceart.000webhostapp.com/user"; // URL base para consulta
 
-    /**
-     * Preenche todos os atributos da classe
-     * @param object Atributos da classe em objeto literal
-     * @returns User 
-     */
-    factory(user) {
-        this.id = user.id;
-        this.index = user.index;
-        this.name = user.name;
-        this.email = user.email;
-        this.password = user.password;
-        this.phone = user.phone;
-        this.location = {
-            cep: user.cep,
-            state: user.state,
-            city: user.city
-        }
+  /**
+   * Preenche todos os atributos da classe
+   * @param object Atributos da classe em objeto literal
+   * @returns User
+   */
+  factory(user) {
+    this.id = user.id;
+    this.index = user.index;
+    this.name = user.name;
+    this.email = user.email;
+    this.password = user.password;
+    this.phone = user.phone;
+    this.location = {
+      cep: user.cep,
+      state: user.state,
+      city: user.city,
+    };
 
-        this.image = user.image;
-        return this;
+    this.image = user.image;
+    return this;
+  }
+
+  /**
+   * Consulta dados de identificação
+   * @param string email
+   * @param string password
+   * @returns object
+   */
+  async signIn(email = this.email, password = this.password) {
+    let response = await api.get(
+      `${this.url}?email=${email}&password=${password}`
+    );
+
+    if (response.status !== status.OK) {
+      // Executa caso o código da resposta não seja OK
+      HTTPRequestError.throw(response.statusText); // Emite exceção
     }
 
-    /**
-     * Consulta dados de identificação
-     * @param string email
-     * @param string password
-     * @returns object
-     */
-    async signIn(
-        email = this.email,
-        password = this.password
-    ) {
-        let response = await api.get(
-            `${this.url}?email=${email}&password=${password}`
-        );
+    response = response.data;
 
-        if (response.status !== status.OK) { // Executa caso o código da resposta não seja OK
-            HTTPRequestError.throw(response.statusText); // Emite exceção
-        }
+    return {
+      id: response.id,
+      token: response.token,
+      index: response.index,
+      type: response.type,
+    };
+  }
 
-        response = response.data;
+  /**
+   * Consulta dados gerais
+   * @param boolean true para obter dados sigilosos
+   * @returns object
+   */
+  async fetch(isToken = false) {
+    let response = await api.get(
+      `${this.url}?id=${this.id}&token=${isToken}&type=${this.type}`
+    );
 
-        return {
-            id: response.id,
-            token: response.token,
-            index: response.index,
-            type: response.type
-        }
+    if (response.status !== status.OK) {
+      HTTPRequestError.throw(response.statusText);
     }
 
-    /**
-     * Consulta dados gerais
-     * @param boolean true para obter dados sigilosos
-     * @returns object 
-     */
-    async fetch(isToken = false) {
-        let response = await api.get(
-            `${this.url}?id=${this.id}&token=${isToken}&type=${this.type}`
-        );
+    return response.data;
+  }
 
-        if (response.status !== status.OK) {
-            HTTPRequestError.throw(response.statusText);
-        }
+  /**
+   * Busca lista de usuários
+   * @param int posição de início da consulta
+   * @param int limite de itens retornados
+   * @param string filtro de busca
+   * @param object dados associados ao filtro (nome, localização e etc)
+   * @returns object[]
+   */
+  async fetchList(offset = 0, limit = 25, filter = null, data = {}) {
+    let url = `${this.url}/list?offset=${offset}&limit=${limit}&type=${this.type}`;
 
-        return response.data;
+    if (filter === "name") {
+      url += `&name=${data.name}`;
+    } else if (filter === "location") {
+      url += `&state=${data.state}&city=${data.city}`;
     }
 
-    /**
-     * Busca lista de usuários
-     * @param int posição de início da consulta
-     * @param int limite de itens retornados
-     * @param string filtro de busca
-     * @param object dados associados ao filtro (nome, localização e etc)
-     * @returns object[]
-     */
-    async fetchList(offset = 0, limit = 25, filter = null, data = {}) {
-        let url = `${this.url}/list?offset=${offset}&limit=${limit}&type=${this.type}`;
+    let response = await api.get(url);
 
-        if (filter === 'name') {
-            url += `&name=${data.name}`;
-        } else if (filter === 'location') {
-            url += `&state=${data.state}&city=${data.city}`;
-        }
-
-        let response = await api.get(url);
-
-        if (response.status !== status.OK) {
-            HTTPRequestError.throw(response.statusText);
-        }
-
-        return response.data;
+    if (response.status !== status.OK) {
+      HTTPRequestError.throw(response.statusText);
     }
 
-    /**
-     * Atualiza lista de atributos
-     * @param string token
-     * @param object[] lista de atributos a serem atualizados
-     * @returns string[] lista de exceções obtidas
-     */
-    updateList(token, attributes = [{ name: null, value: null }]) {
-        let errors = []
-        attributes.forEach(column => { // itera colunas a serem atualizadas
-            try {
-                this.update({ // atualiza dados da api
-                    id: token,
-                    type: this.type,
-                    column: column.name,
-                    info: column.value
-                });
+    return response.data;
+  }
 
-                this[column.name] = column.value; // Atualiza dados localmente
-            } catch (error) {
-                errors
-                    .push( // Guarda exceção para ser retornada
-                        `Não foi possível atualizar o item ${column.name} com o valor ${column.value}`
-                    );
-            }
-
+  /**
+   * Atualiza lista de atributos
+   * @param string token
+   * @param object[] lista de atributos a serem atualizados
+   * @returns string[] lista de exceções obtidas
+   */
+  updateList(token, attributes = [{ name: null, value: null }]) {
+    let errors = [];
+    attributes.forEach((column) => {
+      // itera colunas a serem atualizadas
+      try {
+        this.update({
+          // atualiza dados da api
+          id: token,
+          type: this.type,
+          column: column.name,
+          info: column.value,
         });
 
-        return errors;
-    }
-
-    /**
-     * Atualiza um atributo específico
-     * @param object body da requisição
-     */
-    async update(data) {
-        let response = await api.post(
-            `${this.url}/update`, data
+        this[column.name] = column.value; // Atualiza dados localmente
+      } catch (error) {
+        errors.push(
+          // Guarda exceção para ser retornada
+          `Não foi possível atualizar o item ${column.name} com o valor ${column.value}`
         );
+      }
+    });
 
-        if (response.status !== status.NO_CONTENT) {
-            HTTPRequestError.throw(response.statusText);
-        }
+    return errors;
+  }
+
+  /**
+   * Atualiza um atributo específico
+   * @param object body da requisição
+   */
+  async update(data) {
+    let response = await api.post(`${this.url}/update`, data);
+
+    if (response.status !== status.NO_CONTENT) {
+      HTTPRequestError.throw(response.statusText);
     }
+  }
 
-    /**
-     * Deleta usuário
-     * @param string token
-     */
-    async delete(token) {
-        let response = await api.get(
-            `${this.url}/delete`, { id: token }
-        );
+  /**
+   * Deleta usuário
+   * @param string token
+   */
+  async delete(token) {
+    let response = await api.get(`${this.url}/delete`, { id: token });
 
-        if (response.status !== status.NO_CONTENT) {
-            HTTPRequestError.throw(response.statusText);
-        }
+    if (response.status !== status.NO_CONTENT) {
+      HTTPRequestError.throw(response.statusText);
     }
-
+  }
 }
 
 export class Artist extends User {
-    cpf; art; wage; birthday;
+  cpf;
+  art;
+  wage;
+  birthday;
 
-    constructor() {
-        super();
-        this.type = 'artist';
+  constructor() {
+    super();
+    this.type = "artist";
+  }
+
+  factory(artist) {
+    super.factory(artist);
+    this.wage = artist.wage;
+    this.cpf = artist.cpf;
+    this.art = artist.art;
+
+    return this;
+  }
+
+  /**
+   * Cadastra um usuário
+   */
+  async signUp() {
+    let response = await api.post(this.url, {
+      name: this.name,
+      image: this.image,
+      type: this.type,
+      phone: this.phone,
+      email: this.email,
+      password: this.password,
+      birthday: this.birthday,
+      cpf: this.cpf,
+      cep: this.location.cep,
+      state: this.location.state,
+      city: this.location.city,
+      art: this.art,
+      wage: this.wage,
+    });
+
+    if (response.status !== status.CREATED) {
+      HTTPRequestError.throw((await response).statusText);
     }
+  }
 
-    factory(artist) {
+  fetch(token = false) {
+    return new Artist().factory(super.fetch(token)); // Intancia o retorno
+  }
 
-        super.factory(artist);
-        this.wage = artist.wage;
-        this.cpf = artist.cpf;
-        this.art = artist.art;
-
-        return this;
-    }
-
-    /**
-     * Cadastra um usuário
-     */
-    async signUp() {
-        let response = await api.post(
-            this.url,
-            {
-                name: this.name,
-                image: this.image,
-                type: this.type,
-                phone: this.phone,
-                email: this.email,
-                password: this.password,
-                birthday: this.birthday,
-                cpf: this.cpf,
-                cep: this.location.cep,
-                state: this.location.state,
-                city: this.location.city,
-                art: this.art,
-                wage: this.wage
-            });
-
-        if (response.status !== status.CREATED) {
-            HTTPRequestError.throw((await response).statusText);
-        }
-    }
-
-
-    fetch(token = false) {
-        return new Artist().factory(super.fetch(token)); // Intancia o retorno
-    }
-
-    fetchList(offset = 0, limit = 25, filter = null, data = {}) {
-
-        return super
-            .fetchList(offset, limit, filter, data)
-            .map(new Artist().factory); // Instancia todos os registor retornados
-    }
+  fetchList(offset = 0, limit = 25, filter = null, data = {}) {
+    return super
+      .fetchList(offset, limit, filter, data)
+      .map(new Artist().factory); // Instancia todos os registor retornados
+  }
 }
 
 export class Enterprise extends User {
-    cnpj; companyName;
+  cnpj;
+  companyName;
 
-    constructor() {
-        super();
-        this.type = 'enterprise';
+  constructor() {
+    super();
+    this.type = "enterprise";
+  }
+
+  factory(enterprise = {}) {
+    super.factory(enterprise);
+    this.cnpj = enterprise.cnpj;
+    this.companyName = enterprise.companyName;
+    this.location.neighborhood = enterprise.neighborhood;
+    this.location.adress = enterprise.adress;
+
+    return this;
+  }
+
+  /**
+   * Cadastra um usuário
+   */
+  async signUp() {
+    let response = await api.post(this.url, {
+      name: this.name,
+      companyName: this.companyName,
+      image: this.image,
+      phone: this.phone,
+      type: this.type,
+      email: this.email,
+      password: this.password,
+      birthday: this.birthday,
+      cnpj: this.cnpj,
+      cep: this.location.cep,
+      state: this.location.state,
+      city: this.location.city,
+      neighborhood: this.location.neighborhood,
+      address: this.location.address,
+    });
+
+    if (response.status !== status.CREATED) {
+      HTTPRequestError.throw((await response).statusText);
     }
+  }
 
-    factory(enterprise = {}) {
-        super.factory(enterprise);
-        this.cnpj = enterprise.cnpj;
-        this.companyName = enterprise.companyName;
-        this.location.neighborhood = enterprise.neighborhood;
-        this.location.adress = enterprise.adress;
+  fetch(token = false) {
+    return new Artist().factory(super.fetch(token)); // Intancia o retorno
+  }
 
-        return this;
-    }
-
-    /**
-     * Cadastra um usuário
-     */
-    async signUp() {
-        let response = await api.post(
-            this.url,
-            {
-                name: this.name,
-                companyName: this.companyName,
-                image: this.image,
-                phone: this.phone,
-                type: this.type,
-                email: this.email,
-                password: this.password,
-                birthday: this.birthday,
-                cnpj: this.cnpj,
-                cep: this.location.cep,
-                state: this.location.state,
-                city: this.location.city,
-                neighborhood: this.location.neighborhood,
-                address: this.location.address
-            });
-
-        if (response.status !== status.CREATED) {
-            HTTPRequestError.throw((await response).statusText);
-        }
-    }
-
-    fetch(token = false) {
-        return new Artist().factory(super.fetch(token)); // Intancia o retorno
-    }
-
-    fetchList(offset = 0, limit = 25, filter = null, data = {}) {
-
-        return super
-            .fetchList(offset, limit, filter, data)
-            .map(new Artist().factory); // Instancia todos os registor retornados
-    }
+  fetchList(offset = 0, limit = 25, filter = null, data = {}) {
+    return super
+      .fetchList(offset, limit, filter, data)
+      .map(new Artist().factory); // Instancia todos os registor retornados
+  }
 }
