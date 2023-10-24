734899fd1ff098730e1cd6b17a7746a12382e485
CLAUDIO RIBEIRO DE SOUSA JUNIOR <claudiorevista@hotmail.com>
2023-10-24 16:41:22 -0300

    Add profile update modal

---
+++ src/components/profileUpdate/profileUpdate.tsx
@@ -0,0 +1,61 @@
+import {
+  FormInputButton,
+  FormInputFullField,
+  FormInputHalfField,
+  FormInputTextbox,
+  HeaderLogo,
+  Icon,
+  InnerContainer,
+  MainSignUpContainer,
+  ModalContainer,
+  SignContainer,
+} from "./profileUpdateStyles";
+import XIcon from "../../assets/x.svg";
+import { useContext } from "react";
+import { ModalContext } from "../../contexts/ModalContext";
+
+function ProfileUpdate() {
+  const { hideModal, setHideModal } = useContext(ModalContext);
+
+  return (
+    <ModalContainer>
+      <MainSignUpContainer>
+        <InnerContainer>
+          <HeaderLogo>
+            <Icon
+              alt="X"
+              src={XIcon}
+              onClick={() => setHideModal(!hideModal)}
+            />
+            <h1>Alterar perfil</h1>
+            <span>
+              Altere todos os campos que deseja atualizar. Caso não tenha
+              encontrado o campo desejado, isso significa que o dado informado
+              não pode ser modificado por questão de segurança.
+            </span>
+          </HeaderLogo>
+          <SignContainer>
+            <FormInputHalfField type="text" placeholder="Nome artístico" />
+            <FormInputHalfField type="tel" placeholder="Telefone" />
+            <FormInputFullField type="text" placeholder="Foto de perfil" />
+            <FormInputFullField type="text" placeholder="Arte" />
+            <FormInputFullField type="text" placeholder="Descrição" />
+            <FormInputFullField type="text" placeholder="CEP" />
+            <FormInputHalfField type="text" placeholder="Cidade" />
+            <FormInputHalfField
+              type="text"
+              placeholder="UF"
+            />
+            <FormInputFullField type="text" placeholder="Bairro" />
+            <FormInputFullField type="text" placeholder="Endereço" />
+            <FormInputHalfField type="password" placeholder="Nova senha" />
+            <FormInputHalfField type="password" placeholder="Repita a nova senha" />
+            <FormInputButton>Salvar Alterações</FormInputButton>
+          </SignContainer>
+        </InnerContainer>
+      </MainSignUpContainer>
+    </ModalContainer>
+  );
+}
+
+export default ProfileUpdate;
--- src/pages/modalTest/modalTest.tsx
+++ src/pages/modalTest/modalTest.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Modal, ModalButton } from "./modalTestStyles";
 import { ModalContext } from "../../contexts/ModalContext";
-import NewSelection from "../../components/newSelection/newSelection";
+import ProfileUpdate from "../../components/profileUpdate/profileUpdate";
 
 function ModalTest() {
   const { hideModal, setHideModal } = useContext(ModalContext);
@@ -10,7 +10,7 @@ function ModalTest() {
     <>
       <ModalButton onClick={() => setHideModal(!hideModal)} />
       <Modal hideModal={hideModal}>
-        <NewSelection />
+        <ProfileUpdate />
       </Modal>
     </>
   );
