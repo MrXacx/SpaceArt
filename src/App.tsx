f7b7ee44c72d2f2c4f9646cf8f870bdbabac3245
MrXacx <misterxacx@gmail.com>
2023-11-03 19:04:35 -0300

    feat: aplica SearchProvider em toda a aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,28 @@ import Profile from "./pages/profile/profile";
 import Search from "./pages/search/search";
 import Modal from "./pages/modal/modal";
 import ModalTest from "./pages/modalTest/modalTest";
-
+import { SearchProvider } from "./contexts/SearchContext";
 import { UserStorage } from "./contexts/UserContext";
 
 
 function App() {
   return (
     <BrowserRouter>
-
       <UserStorage>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up-enterprise" element={<SignUpEnterprise />} />
-          <Route path="/sign-up-artist" element={<SignUpArtist />} />
-          <Route path="/config" element={<Config />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/modal" element={<Modal />} />
-          <Route path="/modal-test" element={<ModalTest />} />
-        </Routes>
+        <SearchProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up-enterprise" element={<SignUpEnterprise />} />
+            <Route path="/sign-up-artist" element={<SignUpArtist />} />
+            <Route path="/config" element={<Config />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/modal" element={<Modal />} />
+            <Route path="/modal-test" element={<ModalTest />} />
+          </Routes>
+        </SearchProvider>
       </UserStorage>
     </BrowserRouter>
   );
