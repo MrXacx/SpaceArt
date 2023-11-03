e3b44dcdd654a4b8ff33d55a921b72f3a8cde50a
MrXacx <misterxacx@gmail.com>
2023-11-03 19:05:14 -0300

    feat: adapta página de pesquisa para diferentes modalidades de busca

--- src/pages/search/search.tsx
+++ src/pages/search/search.tsx
@@ -1,13 +1,101 @@
+import { useState, useContext } from "react";
 import HeaderLogged from "../../components/headerLogged/headerLogged";
-import SearchArtist from "../../components/searchArtist/searchArtist";
-import { Spacing } from "./searchStyles";
+import {
+  Spacing,
+  ChooseFilterContainer,
+  FilterOptionsContainer,
+  FilterOption,
+  FilterBarContainer,
+  FilterBarItem,
+  SearchResultContainer,
+  CardProfileContainer,
+} from "./searchStyles";
+import LocationFilterBar from "../../components/locationFilterBar/locationFilterBar";
+import NameFilterBar from "../../components/nameFilterBar/nameFilterBar";
+import { SearchContext } from "../../contexts/SearchContext";
+import CardProfile from "../../components/cardProfile/cardProfile";
+import { UserContext } from "../../contexts/UserContext";
+import { AccountType } from "../../enums/AccountType";
 
 function Search() {
+  const [isNameFilterSelected, setNameFilterSelected] = useState("true");
+  const [isLocationFilterSelected, setLocationFilterSelected] = useState("false");
+  const [isArtistSearched, setArtistSearched] = useState("true");
+  const [isEnterpriseSearched, setEnterpriseSearched] = useState("false");
+
+  const { cardsData, setTypeSearched } = useContext(SearchContext);
+
+  const { type } = useContext(UserContext);
+
+  // eslint-disable-next-line eqeqeq
+  setTypeSearched(type == AccountType.artist ? AccountType.enterprise : AccountType.artist)
+
+
   return (
     <>
       <HeaderLogged />
       <Spacing />
-      <SearchArtist />
+      <ChooseFilterContainer>
+        <div>
+          <span>BUSCAR POR</span>
+          <FilterOptionsContainer>
+            <FilterOption
+              selected={isArtistSearched}
+              onClick={() => {
+                setArtistSearched("true");
+                setEnterpriseSearched("false");
+              }}
+            >
+              ARTISTA
+            </FilterOption>
+            <FilterOption
+              selected={isEnterpriseSearched}
+              onClick={() => {
+                setEnterpriseSearched("true");
+                setArtistSearched("false");
+              }}
+            >
+              EMPRESA
+            </FilterOption>
+          </FilterOptionsContainer>
+        </div>
+        <div>
+          <span>FILTRAR POR</span>
+          <FilterOptionsContainer>
+            <FilterOption
+              selected={isNameFilterSelected}
+              onClick={() => {
+                setNameFilterSelected("true");
+                setLocationFilterSelected("false");
+              }}
+            >
+              NOME
+            </FilterOption>
+            <FilterOption
+              selected={isLocationFilterSelected}
+              onClick={() => {
+                setLocationFilterSelected("true");
+                setNameFilterSelected("false");
+              }}
+            >
+              LOCALIZAÇÃO
+            </FilterOption>
+          </FilterOptionsContainer>
+        </div>
+      </ChooseFilterContainer>
+      <FilterBarContainer>
+        <FilterBarItem is_visible={isNameFilterSelected}>
+          <NameFilterBar withArtField={isArtistSearched} />
+        </FilterBarItem>
+        <FilterBarItem is_visible={isLocationFilterSelected}>
+          <LocationFilterBar withArtField={isArtistSearched} />
+        </FilterBarItem>
+      </FilterBarContainer>
+      <SearchResultContainer>
+        <CardProfileContainer>
+          {cardsData.map((card: any) => CardProfile)}
+        </CardProfileContainer>
+      </SearchResultContainer>
     </>
   );
 }
--- src/pages/search/searchStyles.ts
+++ src/pages/search/searchStyles.ts
@@ -4,3 +4,115 @@ export const Spacing = styled.div`
   height: 100px;
   background-color: #fff;
 `;
+
+export const ChooseFilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;  
+  margin: 1rem auto;
+  width: 80%;
+
+  >div {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;  
+    margin: 1rem auto;
+
+    span {
+      font-weight: 800;
+      width: 100%;
+      text-align: center;
+      font-size: 1.5rem;
+    }
+  
+  }
+  
+  @media (min-width: 960px){
+    flex-direction: row;
+    justify-content: space-evenly;
+    
+    span {
+      margin-bottom: .5rem;
+    }
+  }
+`;
+
+export const FilterOptionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  width: 66%;
+  
+`;
+
+export const FilterOption = styled.button<{ selected: string }>`
+  width: 100%;
+  padding: 0.5rem;
+  margin: .5rem;
+  border-radius: 5px;
+  border: none;
+  box-shadow: none;
+  cursor: pointer;
+  font-weight: 700;
+  background-color: ${(e) => JSON.parse(e.selected) ? '#ff8311' : 'transparent'};
+  transition: .3s;
+  font-size: 1.3rem;
+  
+  color: ${(e) => JSON.parse(e.selected) ? '#fff' : '#ff8311'};
+`;
+
+export const FilterBarContainer = styled.div`
+  width: 100%;
+  display: grid;
+  place-items: center;
+`;
+
+export const FilterBarItem = styled.div<{ is_visible: string }>`
+  display: ${(e) => JSON.parse(e.is_visible) ? 'grid' : 'none'};
+  width: 100%;
+`;
+
+export const SearchResultContainer = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: #f5f5f5;
+  color: #fa8b08;
+  justify-content: center;
+  flex-direction: column;
+  padding: 25px 0;
+
+  h2 {
+    text-align: center;
+    font-size: 1.25rem;
+  }
+
+  @media (min-width: 768px) {
+    h2 {
+      font-size: 1.5rem;
+    }
+  }
+`;
+
+export const CardProfileContainer = styled.div`
+  display: grid;
+  margin: 0 auto;
+
+  grid-template-columns: 1fr;
+  gap: 4rem;
+  padding: 50px 0;
+    
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (min-width: 960px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 1920px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
