c332979e687fd101b04338eed42a9d468005699b
MrXacx <misterxacx@gmail.com>
2023-11-05 11:54:48 -0300

    feat: aplica parâmetro no antiga rota profile

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,12 @@ import Profile from "./pages/profile/profile";
 import Search from "./pages/search/search";
 import Modal from "./pages/modal/modal";
 import ModalTest from "./pages/modalTest/modalTest";
-import { SearchProvider } from "./contexts/SearchContext";
 import { UserStorage } from "./contexts/UserContext";
-
 
 function App() {
   return (
     <BrowserRouter>
-      <UserStorage>
-        <SearchProvider>
+      <UserStorage>   
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/sign-in" element={<SignIn />} />
@@ -25,12 +22,11 @@ function App() {
             <Route path="/sign-up-artist" element={<SignUpArtist />} />
             <Route path="/config" element={<Config />} />
             <Route path="/feed" element={<Feed />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/user/:index" element={<Profile />} />
             <Route path="/search" element={<Search />} />
             <Route path="/modal" element={<Modal />} />
             <Route path="/modal-test" element={<ModalTest />} />
           </Routes>
-        </SearchProvider>
       </UserStorage>
     </BrowserRouter>
   );
