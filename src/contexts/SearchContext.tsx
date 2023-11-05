400f757a821c4f9037aa757b6a96b0986e0ac841
MrXacx <misterxacx@gmail.com>
2023-11-05 11:54:05 -0300

    feat: aplica tratamento para fetchUsersByLocation

--- src/contexts/SearchContext.tsx
+++ src/contexts/SearchContext.tsx
@@ -1,8 +1,8 @@
 
-import { Artist, Enterprise } from "../api/User";
+import { Artist, Enterprise, User } from "../api/User";
 import { AccountType } from "../enums/AccountType";
 import { BrazilianState } from "../enums/BrazilianState";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 interface SearchStoreProps {
 	children: React.ReactNode;
@@ -11,73 +11,60 @@ interface SearchStoreProps {
 export const SearchContext = createContext({} as any);
 
 export const SearchProvider = ({ children }: SearchStoreProps) => {
-	const [typeSearched, setTypeSearched] = useState<AccountType>();
+
 	const [cardsData, setCardsData] = useState<any[]>([]);
 
-	const fetchRandomUsers = (page = 0, limit = 5) => {
-	
+	const turnListOnCardData = (users: User[]) => {
+
+		return users.map(user => {
+			const data = user.toObject();
+
+			// Obtém art e wage apenas caso a busca envolva artistas
+			const { art, wage } = user instanceof Artist ? user.toObject() : { art: undefined, wage: undefined };
+
+			return {
+				id: data.id,
+				index: data.index,
+				image: data.image,
+				name: data.name,
+				type: data.type,
+				city: data.location?.city,
+				state: data.location?.state,
+				art, wage,
+			}
+		});
+	}
+
+	const fetchRandomUsers = (typeSearched: AccountType, page = 0, limit = 5) => {
+
 		// eslint-disable-next-line eqeqeq
-		const userClient = typeSearched == 'artist' ? new Artist() : new Enterprise();
+		const userClient = typeSearched == AccountType.artist ? new Artist() : new Enterprise();
 		userClient
-		.fetchListWithoutFilter(page, limit)
-		.then(users => users.map( // trata a resposta
-			user => {
-				const data = user.toObject();
-
-				// Obtém art e wage apenas caso a busca envolva artistas
-				const { art, wage } = user instanceof Artist ? user.toObject() : { art: undefined, wage: undefined };
-				return {
-					id: data.id,
-					index: data.index,
-					image: data.image,
-					name: data.name,
-					type: data.type,
-					city: data.location?.city,
-					state: data.location?.state,
-					art, wage,
-				}
-			})
-		)
-		.then(setCardsData);
+			.fetchListWithoutFilter(page, limit)
+			.then(turnListOnCardData)
+			.then(setCardsData)
+			.catch(e => console.error(e));
 	}
 
-	const fetchUsersByName = (name: string, page = 0, limit = 5) => {
-			// eslint-disable-next-line eqeqeq
-			const userClient = typeSearched == 'artist' ? new Artist() : new Enterprise();
-					userClient
-					.fetchListFilteringName(name, page, limit)
-					.then(users => users.map( // trata a resposta
-							user => {
-									const data = user.toObject();
-
-									// Obtém art e wage apenas caso a busca envolva artistas
-									const { art, wage } = user instanceof Artist ? user.toObject() : { art: undefined, wage: undefined };
-									return {
-											id: data.id,
-											index: data.index,
-											image: data.image,
-											name: data.name,
-											type: data.type,
-											city: data.location?.city,
-											state: data.location?.state,
-											art, wage,
-									}
-							})
-					)
-					.then(setCardsData);
-	}
-
-	const fetchUsersByLocation = (state: BrazilianState, city:string, page = 0, limit = 5) => {
+	const fetchUsersByName = (typeSearched: AccountType, name: string, page = 0, limit = 5) => {
 		// eslint-disable-next-line eqeqeq
-		const userClient = typeSearched == 'artist' ? new Artist() : new Enterprise();
+		const userClient = typeSearched == AccountType.artist ? new Artist() : new Enterprise();
+		userClient
+			.fetchListFilteringName(name, page, limit)
+			.then(turnListOnCardData)
+			.then(setCardsData)
+			.catch(e => console.error(e.message));
+	}
+
+	const fetchUsersByLocation = (typeSearched: AccountType, state: BrazilianState, city: string, page = 0, limit = 5) => {
+		// eslint-disable-next-line eqeqeq
+		const userClient = typeSearched == AccountType.artist ? new Artist() : new Enterprise();
 		userClient
 			.fetchListFilteringLocation(state, city, page, limit)
+			.then(turnListOnCardData)
+			.then(setCardsData)
+			.catch(e => console.error(e.message));
 	}
-
-
-	useEffect(() => {
-		if (cardsData.length === 0) fetchRandomUsers(0, 20);
-	})
 
 	return (
 		<SearchContext.Provider
@@ -86,7 +73,6 @@ export const SearchProvider = ({ children }: SearchStoreProps) => {
 				fetchRandomUsers,
 				fetchUsersByName,
 				fetchUsersByLocation,
-        setTypeSearched,
 			}}
 		>
 			{children}
