d1c54a12a6e9eb9f34dc4bfd17cdf7857b3c4929
MrXacx <misterxacx@gmail.com>
2023-11-05 11:55:51 -0300

    feat: aplica SearchProvider em todas as páginas

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ModalProvider } from "./contexts/ModalContext";
+import { SearchProvider } from "./contexts/SearchContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ModalProvider>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
     </ModalProvider>
   </React.StrictMode>
 );
