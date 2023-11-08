791ae17aec46fdd58dfc2cddac362264a3dabadf
MrXacx <misterxacx@gmail.com>
2023-11-07 23:49:48 -0300

    feat: começa a deixar newContract dinâmico

--- src/components/newContract/newContract.tsx
+++ src/components/newContract/newContract.tsx
@@ -9,12 +9,16 @@ import {
   SignContainer,
 } from "./newContractStyles";
 import XIcon from "../../assets/x.svg";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ModalContext } from "../../contexts/ModalContext";
 import ArtistBoxCheck from "../artistBoxCheck/artistBoxCheck";
+import { ArtType } from "../../enums/ArtType";
 
 function NewContract() {
   const { hideModal, setHideModal } = useContext(ModalContext);
+
+  const [art] = useState<ArtType>();
+  const [price, setPrice] = useState(0.0);
 
   return (
     <ModalContainer>
@@ -23,9 +27,20 @@ function NewContract() {
         <h1>Novo contrato</h1>
       </HeaderLogo>
       <SignContainer>
-        <ArtistBoxCheck />
-        <FormInputFullField type="text" placeholder="Arte" />
-        <FormInputFullField type="text" placeholder="Valor" />
+        <ArtistBoxCheck
+          name={''}
+          image={''}
+          art={ArtType.dance}
+          city={''}
+          state={''}
+        />
+        <FormInputFullField value={art} type="text" placeholder="Arte" disabled/>
+        <FormInputFullField
+        type="number"
+        placeholder="Valor"
+        value={price.toFixed(2)}
+        onChange={(e: any) => setPrice(e.target.value)}
+        />
         <FormInputFullField type="text" placeholder="Data do evento" />
         <FormInputHalfField type="text" placeholder="Horário de início" />
         <FormInputHalfField type="text" placeholder="Horário de encerramento" />
