2f17667dd18212f781d3fa7111858c08b10547ca
MrXacx <misterxacx@gmail.com>
2023-11-09 23:03:27 -0300

    feat: define estados para criação do contrato

--- src/components/newContract/newContract.tsx
+++ src/components/newContract/newContract.tsx
@@ -13,7 +13,8 @@ import { useContext, useState } from "react";
 import { ModalContext } from "../../contexts/ModalContext";
 import { SelectArtistContext } from "../../contexts/SelectArtistContext";
 import ArtistBox from "../artistBox/artistBox";
-import { ArtType } from "../../enums/ArtType";
+import { ArtType, ArtTypesUtil } from "../../enums/ArtType";
+import { agreementSchema } from "../../schemas/services/AgreementSchemas";
 
 function NewContract() {
   const { hideModal, setHideModal } = useContext(ModalContext);
@@ -21,8 +22,28 @@ function NewContract() {
 
   const selectedArtist = artist.toObject();
 
-  const [art] = useState<ArtType>();
-  const [price, setPrice] = useState(0.0);
+  const [art] = useState<ArtType>(ArtTypesUtil.parse(selectedArtist.art));
+  const [price, setPrice] = useState(selectedArtist.wage);
+  const [date, setDate] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [description, setDescription] = useState('');
+  const [inputErrorMessage, setInputErrorMessage] = useState('');
+  const [isValidInput, setInputValidate] = useState('true');
+
+  const createAgreement = () => {
+
+    const { error } = agreementSchema.validate({
+      art, price, date, startTime, endTime, description
+    });
+
+    if(error) {
+      setInputValidate('false');
+      setInputErrorMessage(error.message);
+    } else {
+      // Chama contexto
+    }
+  }
 
   return (
     <ModalContainer>
@@ -30,7 +51,10 @@ function NewContract() {
         <Icon alt="X" src={XIcon} onClick={() => setHideModal(!hideModal)} />
         <h1>Novo contrato</h1>
       </HeaderLogo>
-      <SignContainer>
+      <SignContainer onSubmit={(e: any) => {
+        e.preventDefault();
+        createAgreement();
+      }}>
         <ArtistBox
           name={selectedArtist.name}
           image={selectedArtist.image}
@@ -46,10 +70,30 @@ function NewContract() {
           value={price.toFixed(2)}
           onChange={(e: any) => setPrice(e.target.value)}
         />
-        <FormInputFullField type="text" placeholder="Data do evento" />
-        <FormInputHalfField type="text" placeholder="Horário de início" />
-        <FormInputHalfField type="text" placeholder="Horário de encerramento" />
-        <FormInputTextbox type="textbox" placeholder="Descrição" />
+        <FormInputFullField
+          type="date"
+          placeholder="Data do evento"
+          value={date}
+          onChange={(e: any) => setDate(e.target.value)}
+        />
+        <FormInputHalfField
+          type="time"
+          placeholder="Horário de início"
+          value={startTime}
+          onChange={(e: any) => setStartTime(e.target.value)}
+        />
+        <FormInputHalfField
+          type="time"
+          placeholder="Horário de encerramento"
+          value={endTime}
+          onChange={(e: any) => setEndTime(e.target.value)}
+        />
+        <FormInputTextbox
+          type="textbox"
+          placeholder="Descrição"
+          value={description}
+          onChange={(e: any) => setDescription(e.target.value)}
+        />
         <FormInputButton>CRIAR CONTRATO</FormInputButton>
       </SignContainer>
     </ModalContainer>
