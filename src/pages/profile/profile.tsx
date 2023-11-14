0c6f843f500d32843d5af635844290a3d8e4be2b
MrXacx <misterxacx@gmail.com>
2023-11-14 10:29:08 -0300

    feat: aplica desaparecimentos de itens sem conteúdo e dá mais liberdade para a manipulação do calendário

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -31,6 +31,7 @@ import {
   UserStats,
   UserType,
   Wrapper,
+  UserDetailsItem,
 } from "./profileStyles";
 import ProfileImage from "../../assets/verified.svg";
 import LocationIcon from "../../assets/location.svg";
@@ -43,41 +44,50 @@ import MusicIcon from "../../assets/music.svg";
 import Footer from "../../components/footer/footer";
 import HeaderLogged from "../../components/headerLogged/headerLogged";
 import { UserContext } from "../../contexts/UserContext";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import dayjs from "dayjs";
-import portuguesPlugin from "dayjs/locale/pt-br";
+import portugueseLocale from "dayjs/locale/pt-br";
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import calendar from 'dayjs/plugin/calendar';
 
 function Profile() {
+  dayjs.extend(customParseFormat);
+  dayjs.extend(calendar);
+  dayjs.locale(portugueseLocale);
+
   const params = useParams();
   const { index, user, type, fetchAnotherUser, fetchPostsByUser, fetchAgreementsByUser } = useContext(UserContext);
   const [profileData, setProfileData] = useState<any>({});
   const [posts, setPosts] = useState<any[]>([]);
   const [agreements, setAgreements] = useState<any[]>([]);
-
-
   const [date, setDate] = useState(dayjs().set('date', 1));
   const [selectedDate, selectDate] = useState(dayjs());
 
-  useEffect(() => {
+  const fetchProfileOwner = useCallback(() => {
     if (params.index === index) {
       // Executa caso o index da url condizer com o do usuário logado
       setProfileData(user);
+      return user.id;
+
     } else {
       // Executa caso o perfil seja de outro usuário
-      fetchAnotherUser(params.index) // busca dados do usuário
-        .then(setProfileData);
+      return fetchAnotherUser(params.index) // busca dados do usuário
+        .then((data: any) => {
+          setProfileData(data ?? {})
+          return data.id;
+        });
     }
-  }, [index, user, fetchAnotherUser, setProfileData, params]);
+  }, [fetchAnotherUser, index, params, user]);
+  const fecthPosts = useCallback((id: string) => fetchPostsByUser(id).then(setPosts), [fetchPostsByUser])
+  const fetchAgreement = useCallback((id: string) => fetchAgreementsByUser(id).then(setAgreements), [fetchAgreementsByUser]);
 
   useEffect(() => {
-    if (profileData) {
-      fetchPostsByUser(profileData.id).then(setPosts);
-      fetchAgreementsByUser(profileData.id)
-        .then(setAgreements);
+    fetchProfileOwner()
+      .then((id: string) => Promise.all([fecthPosts(id), fetchAgreement(id)]))
+      .catch((e: any) => console.error(e.message))
 
-    }
-  }, [fetchAgreementsByUser, fetchPostsByUser, profileData]);
+  }, [fetchAgreement, fecthPosts, fetchProfileOwner]);
 
   const iterateCalendar = () => {
     let i = 0;
@@ -125,7 +135,6 @@ function Profile() {
     return days;
   };
 
-
   return (
     <>
       <HeaderLogged />
@@ -150,24 +159,20 @@ function Profile() {
               </UserDetailsHeader>
 
               <ul>
-                <li>
+                <UserDetailsItem hidden={Boolean(profileData.location).toString()}>
                   <Icon src={LocationIcon} alt="localização" />
-                  <span>{`${profileData.location?.city} - ${profileData.location?.state}`}</span>
-                </li>
-                <li>
+                  <span>{`${profileData.location?.city ?? ''} - ${profileData.location?.state ?? ''}`}</span>
+                </UserDetailsItem>
+
+                <UserDetailsItem hidden={Boolean(profileData.rate).toString()}>
                   <Icon src={StarIcon} alt="nota" />
                   <span>{profileData.rate?.toFixed(2)}</span>
-                </li>
-                <li>
+                </UserDetailsItem>
+
+                <UserDetailsItem hidden={Boolean(profileData.website).toString()}>
                   <Icon src={GlobalIcon} alt="site" />
-                  <a
-                    href={profileData.website ?? `/user/${profileData.index}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {profileData.website ?? ""}
-                  </a>
-                </li>
+                  <a href={profileData.website} target="_blank" rel="noreferrer">Meu site</a>
+                </UserDetailsItem>
               </ul>
             </UserDetails>
 
@@ -187,9 +192,9 @@ function Profile() {
           </UserInfo>
         </ProfileContent>
 
-        <DescriptionContainer>
+        <DescriptionContainer is_visible={Boolean(profileData.description).toString()}>
           <span>Descrição</span>
-          <p>{profileData.description ?? ''}</p>
+          <p>{profileData.description}</p>
         </DescriptionContainer>
 
       </ProfileHeader>
@@ -201,19 +206,14 @@ function Profile() {
             <CalendarHeader>
               <MonthNavbar>
                 <button
-                  onClick={() => {
-
-                    if (date.month() > 0) setDate(date.subtract(1, 'month'))
-                  }}>
+                  onClick={() => setDate(date.subtract(1, 'month'))}>
                   {"<"}
                 </button>
 
-                <span>{date.locale(portuguesPlugin).format('MMMM')}</span>
+                <span>{date.format(dayjs().isSame(date, 'year') ? 'MMMM' : 'MMMM/YYYY')}</span>
 
                 <button
-                  onClick={() => {
-                    if (date.month() < 11) setDate(date.add(1, 'month'))
-                  }}>
+                  onClick={() => setDate(date.add(1, 'month'))}>
                   {">"}
                 </button>
               </MonthNavbar>
@@ -232,19 +232,33 @@ function Profile() {
             <CalendarNumberContainer>{iterateCalendar()}</CalendarNumberContainer>
           </Calendar>
           <JobsDayContainer>
-            <DateHeader>{selectedDate.locale(portuguesPlugin).format('MMMM, [DIA] DD')}</DateHeader>
+            <DateHeader>{
+              dayjs(selectedDate).calendar(null, {
+                sameDay: '[Hoje]',
+                nextDay: '[Amanhã] ',
+                lastDay: '[Ontem]',
+                nextWeek: 'DD/MM/YYYY',
+                lastWeek: 'DD/MM/YYYY',
+                sameElse: 'DD/MM/YYYY'
+              })
+            }</DateHeader>
 
             <JobWrapper>
               <Jobs>
-                {agreements.map(
-                  (item: any) =>
-                    <JobInfo>
-                      <JobHour>{item.time.join(' - ')}</JobHour>
-                      <TypeJobIcon>
-                        <Icon alt={item.art} src={MusicIcon} />
-                      </TypeJobIcon>
-                    </JobInfo>
-                )}
+                {agreements
+                  .filter((item: any) => selectedDate.isSame(
+                    dayjs(item.date, 'DD/MM/YYYY'), 'date')
+                  )
+                  .sort((a: any, b: any) => dayjs(a.time.start, 'HH:mm').isBefore(dayjs(b.time.start, 'HH:mm')) ? -1 : 1)
+                  .map(
+                    (item: any) =>
+                      <JobInfo>
+                        <JobHour>{`${item.time.start} - ${item.time.end}`}</JobHour>
+                        <TypeJobIcon>
+                          <Icon alt={item.art} src={MusicIcon} />
+                        </TypeJobIcon>
+                      </JobInfo>
+                  )}
 
               </Jobs>
             </JobWrapper>
