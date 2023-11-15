b680754381cfe25fe5f354cf6e63d74185194e7b
MrXacx <misterxacx@gmail.com>
2023-11-14 23:36:48 -0300

    feat: invoca modals em services

--- src/pages/services/services.tsx
+++ src/pages/services/services.tsx
@@ -3,7 +3,6 @@ import {
   BoxContainer,
   ServicesContainer,
   MainContainer,
-  Modal,
 } from "./servicesStyles";
 import ArrowIcon from "../../assets/arrow.png";
 import Footer from "../../components/footer/footer";
@@ -19,54 +18,25 @@ import { useContext, useState } from "react";
 function Services() {
 
   // Criar alternativa para conter mais de um modal na mesma página
-  const { hideModal, setHideModal } = useContext(ModalContext);
-
-  const [hideNewAgreement, setHideNewAgreement] = useState(true);
-  const [hideCurrentAgreements, setHideCurrentAgreements] = useState(true);
-  const [hidePendingAgreements, setHidePendingAgreements] = useState(true);
-  const [hideAgreementHistory, setHideAgreementHistory] = useState(true);
-  const [hideNewSelection, setHideNewSelection] = useState(true);
-  const [hideCurrentSelections, setHideCurrentSelections] = useState(true);
-  const [hideSelectionHistory, setHideSelectionHistory] = useState(true);
-  const [hideFetchSelections, setHideFetchSelections] = useState(true);
+  const { toogleNewContractVisibility, toogleNewSelectionVisibility, toogleMyContractVisibility, toogleMySelectionVisibility } = useContext(ModalContext);
 
   const agreementServices = [
-    { title: 'Criar contrato', control: hideNewAgreement, handler: setHideNewAgreement },
-    { title: 'Contratos ativos', control: hideCurrentAgreements, handler: setHideCurrentAgreements },
-    { title: 'Contratos pendentes', control: hidePendingAgreements, handler: setHidePendingAgreements },
-    { title: 'Histórico', control: hideAgreementHistory, handler: setHideAgreementHistory },
+    { title: 'Criar contrato', toogle: toogleNewContractVisibility },
+    { title: 'Contratos ativos', toogle: toogleMyContractVisibility },
+    { title: 'Contratos pendentes', toogle: toogleMyContractVisibility },
+    { title: 'Histórico', toogle: toogleMyContractVisibility },
   ]
 
   const selectionServices = [
-    { title: 'Criar seleção', control: hideNewSelection, handler: setHideNewSelection },
-    { title: 'Seleções ativas', control: hideCurrentSelections, handler: setHideCurrentSelections },
-    { title: 'Histórico', control: hideSelectionHistory, handler: setHideSelectionHistory },
-    { title: 'Buscar seleções', control: hideFetchSelections, handler: setHideFetchSelections },
+    { title: 'Criar seleção', toogle: toogleNewSelectionVisibility },
+    { title: 'Seleções ativas', toogle: toogleMySelectionVisibility },
+    { title: 'Histórico', toogle: toogleMySelectionVisibility },
+    { title: 'Buscar seleções', toogle: toogleMySelectionVisibility },
   ]
 
   return (
     <>
       <HeaderLogged />
-
-      <Modal hideModal={hideNewAgreement && hideModal}>
-        <SelectArtist />
-        <NewContract />
-      </Modal >
-
-      <Modal hideModal={hideCurrentAgreements && hideModal}>
-        <MyContract />
-      </Modal >
-
-      <Modal hideModal={hideNewSelection && hideModal}>
-        <NewSelection />
-      </Modal >
-
-      <Modal hideModal={hideCurrentSelections && hideModal}>
-        <MySelection />
-      </Modal >
-
-
-
       <MainContainer>
         <BoxContainer>
 
@@ -79,10 +49,7 @@ function Services() {
                   <img
                     alt={service.title}
                     src={ArrowIcon}
-                    onClick={() => {
-                      setHideModal(true)
-                      service.handler(!service.control)
-                    }}
+                    onClick={() => service.toogle()}
                   />
                 </ArrowContainer>
             )}
@@ -97,7 +64,7 @@ function Services() {
                   <img
                     alt={service.title}
                     src={ArrowIcon}
-                    onClick={() => setHideModal(!hideModal)}
+                    onClick={() => service.toogle()}
                   />
                 </ArrowContainer>
             )}
@@ -106,6 +73,12 @@ function Services() {
         </BoxContainer>
       </MainContainer>
       <Footer />
+
+      <NewContract />
+      <MyContract />
+      <NewSelection />
+      <MySelection />
+
     </>
   );
 }
