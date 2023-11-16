9901bc738fb9d27bd32ae02da114105879fc8d8c
MrXacx <misterxacx@gmail.com>
2023-11-16 18:35:35 -0300

    feat: cria contexto para exibição de dados associados a uma seleção

---
+++ src/contexts/SelectSelectionContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+import { Selection } from "../api/Selection";
+
+interface SelectSelectionStoreProps {
+  children: React.ReactNode;
+}
+
+export const SelectSelectionContext = createContext({} as any);
+
+export const SelectSelectionProvider = ({ children }: SelectSelectionStoreProps) => {
+
+  const [selection, setSelection] = useState<Selection>();
+
+  return (
+    <SelectSelectionContext.Provider
+      value={{ selection, setSelection }}>
+      {children}
+    </SelectSelectionContext.Provider>
+  );
+};
