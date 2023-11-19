a6b0398e25d85e553037f77a0e40915c7e5597da
MrXacx <misterxacx@gmail.com>
2023-11-18 22:36:00 -0300

    feat: cria contexto de seleção de contrato

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { ModalProvider } from "./contexts/ModalContext";
 import { SearchProvider } from "./contexts/SearchContext";
 import { SelectSelectionProvider } from "./contexts/SelectSelectionContext";
 import { SelectArtistProvider } from "./contexts/SelectArtistContext";
+import { SelectAgreementProvider } from "./contexts/SelectAgreement";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,9 +16,11 @@ root.render(
     <ModalProvider>
       <SearchProvider>
         <SelectSelectionProvider>
-          <SelectArtistProvider>
-            <App />
-          </SelectArtistProvider>
+          <SelectAgreementProvider>
+            <SelectArtistProvider>
+              <App />
+            </SelectArtistProvider>
+          </SelectAgreementProvider>
         </SelectSelectionProvider>
       </SearchProvider>
     </ModalProvider>
