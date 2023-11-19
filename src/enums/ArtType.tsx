4e1b9326ee6eb20f03b93c8e27e200256ba06706
MrXacx <misterxacx@gmail.com>
2023-11-19 15:05:48 -0300

    fix: aplica acentuação em música

--- src/enums/ArtType.tsx
+++ src/enums/ArtType.tsx
@@ -1,20 +1,38 @@
 import { NotFoundEnumValueError } from "../errors/NotFoundEnumValueError";
 
-export enum ArtType { dance = "dança", sculpture = "escultura", music = "musica", painting = "pintura", theater = "teatro" };
+export enum ArtType {
+  dance = "dança",
+  sculpture = "escultura",
+  music = "música",
+  painting = "pintura",
+  theater = "teatro",
+}
 
 export class ArtTypesUtil {
-	static parse(artType: string): ArtType | never {
-		switch (artType) {
-			case 'dança': return ArtType.dance;
-			case 'escultura': return ArtType.sculpture;
-			case 'música': return ArtType.music;
-			case 'pintura': return ArtType.painting;
-			case 'teatro': return ArtType.theater;
-			default: return NotFoundEnumValueError.throw();
-		}
-	}
+  static parse(artType: string): ArtType | never {
+    switch (artType) {
+      case "dança":
+        return ArtType.dance;
+      case "escultura":
+        return ArtType.sculpture;
+      case "música":
+        return ArtType.music;
+      case "pintura":
+        return ArtType.painting;
+      case "teatro":
+        return ArtType.theater;
+      default:
+        return NotFoundEnumValueError.throw();
+    }
+  }
 
-	static values(): ArtType[] {
-		return [ArtType.dance, ArtType.sculpture, ArtType.music, ArtType.painting, ArtType.theater];
-	}
+  static values(): ArtType[] {
+    return [
+      ArtType.dance,
+      ArtType.sculpture,
+      ArtType.music,
+      ArtType.painting,
+      ArtType.theater,
+    ];
+  }
 }
