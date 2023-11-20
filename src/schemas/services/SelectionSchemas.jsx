89e1e978ed4faec8f6f29e11583778bab459cf08
MrXacx <misterxacx@gmail.com>
2023-11-20 13:19:59 -0300

    insere title no schema de seleção

--- src/schemas/services/SelectionSchemas.jsx
+++ src/schemas/services/SelectionSchemas.jsx
@@ -6,11 +6,13 @@ const Joi = JoiBase.extend(JoiDate);
 export const selectionSchema = Joi.object({
   price: Joi.number().precision(2).required(),
 
+  title: Joi.string().required().max(30),
+
   art: Joi.string().required(),
 
   initialDate: Joi.date().format("DD/MM/YYYY").required(),
 
-  finallDate: Joi.date().format("DD/MM/YYYY").required(),
+  finalDate: Joi.date().format("DD/MM/YYYY").required(),
 
   initialTime: Joi.date().format("HH:mm").required(),
 
