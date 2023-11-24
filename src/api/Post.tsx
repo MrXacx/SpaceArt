1a59455b2c3902a11114aaf09433bc2ae9acedb6
MrXacx <misterxacx@gmail.com>
2023-11-24 11:03:40 -0300

    fix: insere & como separador de parâmetros da url de posts referenciados por autor

--- src/api/Post.tsx
+++ src/api/Post.tsx
@@ -1,7 +1,6 @@
 import { User } from "./User";
 import { IndexedAPIClient, APIClientFactory } from "./abstracts/APIClient";
 
-
 export class Post extends IndexedAPIClient implements APIClientFactory {
   private author: User | undefined;
   private message: string | undefined;
@@ -13,11 +12,11 @@ export class Post extends IndexedAPIClient implements APIClientFactory {
   factory = () => new Post();
 
   build(post: {
-    id?: string,
-    post_time?: string,
-    author?: User,
-    message?: string,
-    media?: string,
+    id?: string;
+    post_time?: string;
+    author?: User;
+    message?: string;
+    media?: string;
   }): this {
     this.id = post.id;
     this.postTime = post.post_time;
@@ -32,44 +31,51 @@ export class Post extends IndexedAPIClient implements APIClientFactory {
    * Cria a postagem
    */
   async create() {
-    let response = await this.request.post(this.path, JSON.stringify({
-      author: this.author?.getID(),
-      message: this.message as string,
-      media: this.media as string,
-    }));
+    let response = await this.request.post(
+      this.path,
+      JSON.stringify({
+        author: this.author?.getID(),
+        message: this.message as string,
+        media: this.media as string,
+      })
+    );
 
     if (response.status !== Post.httpStatusCode.CREATED) {
-      Post.errorTypes
-        .HTTPRequestError.throw("Não foi possível publicar uma postagem");
+      Post.errorTypes.HTTPRequestError.throw(
+        "Não foi possível publicar uma postagem"
+      );
     }
   }
 
-  /** 
+  /**
    * Busca a postagem
    */
   async fetch() {
     let response = await this.request.get(`${this.path}?id=${this.id}`);
 
     if (response.status !== Post.httpStatusCode.OK) {
-      Post.errorTypes
-        .HTTPRequestError.throw(`Não foi possível buscar a postagem ${this.id}`);
+      Post.errorTypes.HTTPRequestError.throw(
+        `Não foi possível buscar a postagem ${this.id}`
+      );
     }
 
     const data = JSON.parse(response.data);
     data.author = new User(data.author);
     return this.factory().build(data);
-
   }
 
   /**
    * Busca uma lista de postagens
    */
   async fetchList(offset = 0, limit = 25) {
-    let response = await this.request.get(`${this.path}/list?offset=${offset}&limit=${limit}`);
+    let response = await this.request.get(
+      `${this.path}/list?offset=${offset}&limit=${limit}`
+    );
 
     if (response.status !== Post.httpStatusCode.OK) {
-      Post.errorTypes
-        .HTTPRequestError.throw("Não foi possível buscar uma lista de postagens");
+      Post.errorTypes.HTTPRequestError.throw(
+        "Não foi possível buscar uma lista de postagens"
+      );
     }
 
     return JSON.parse(response.data).map((post: any) => {
@@ -79,14 +85,19 @@ export class Post extends IndexedAPIClient implements APIClientFactory {
   }
 
   /**
-  * Busca uma lista de postagens associadas a um usuário
-  */
+   * Busca uma lista de postagens associadas a um usuário
+   */
   async fetchListByAuthor(offset = 0, limit = 25) {
-    let response = await this.request.get(`${this.path}/list?references=author&author=${this.author?.getID()}offset=${offset}&limit=${limit}`);
+    let response = await this.request.get(
+      `${
+        this.path
+      }/list?references=author&author=${this.author?.getID()}&offset=${offset}&limit=${limit}`
+    );
 
     if (response.status !== Post.httpStatusCode.OK) {
-      Post.errorTypes
-        .HTTPRequestError.throw("Não foi possível buscar uma lista de postagens");
+      Post.errorTypes.HTTPRequestError.throw(
+        "Não foi possível buscar uma lista de postagens"
+      );
     }
 
     return JSON.parse(response.data).map((post: any) => {
@@ -99,11 +110,15 @@ export class Post extends IndexedAPIClient implements APIClientFactory {
    * Deleta a postagem
    */
   async delete() {
-    let response = await this.request.post(`${this.path}/delete`, JSON.stringify({ id: this.id }));
+    let response = await this.request.post(
+      `${this.path}/delete`,
+      JSON.stringify({ id: this.id })
+    );
 
     if (response.status !== Post.httpStatusCode.NO_CONTENT) {
-      Post.errorTypes
-        .HTTPRequestError.throw("Não foi possível deletar uma postagem");
+      Post.errorTypes.HTTPRequestError.throw(
+        "Não foi possível deletar uma postagem"
+      );
     }
   }
 
@@ -114,6 +129,6 @@ export class Post extends IndexedAPIClient implements APIClientFactory {
       message: this.message,
       media: this.media,
       postTime: this.postTime,
-    }
+    };
   }
 }
