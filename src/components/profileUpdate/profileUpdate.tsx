9549fcfa0d9d5f7f61f19134c86a5e303a9721f9
MrXacx <misterxacx@gmail.com>
2023-11-23 23:24:08 -0300

    feat: implementa script de atualização em profileUpdate

--- src/components/profileUpdate/profileUpdate.tsx
+++ src/components/profileUpdate/profileUpdate.tsx
@@ -18,17 +18,23 @@ import { ModalContext } from "../../contexts/ModalContext";
 import { ArtTypesUtil } from "../../enums/ArtType";
 import { UserContext } from "../../contexts/UserContext";
 import { AccountType } from "../../enums/AccountType";
+import {
+  profileArtistUpdateSchemas,
+  profileEnterpriseUpdateSchemas,
+} from "../../schemas/user/DataUpdatingSchemas";
 
 function ProfileUpdate() {
   const { hideProfileUpdate, toogleProfileUpdateVisibility } =
     useContext(ModalContext);
-  const { user, type } = useContext(UserContext);
+  const { user, type, updateLoggedUser } = useContext(UserContext);
 
+  const [inputErrorMessage, setInputeErrorMessage] = useState("");
   const [name, setName] = useState(user.name);
-  const [section, setSection] = useState(user.name);
-  const [image, setImage] = useState(user.name);
-  const [description, setDescription] = useState(user.name);
-  const [wage, setWage] = useState(user.name);
+  const [section, setSection] = useState(user.section);
+  const [image, setImage] = useState<any>();
+  const [description, setDescription] = useState(user.description);
+  const [wage, setWage] = useState(user.wage);
+  const [website, setWebsite] = useState(user.website);
   const [art, setArt] = useState(user.art);
 
   const businessSections = [
@@ -40,6 +46,41 @@ function ProfileUpdate() {
     "saúde",
     "transporte",
   ].sort((a: string, b: string) => a.localeCompare(b));
+
+  const update = () => {
+    let items: any = { name, section, description, website, wage, art };
+
+    items =
+      type === AccountType.artist
+        ? { ...items, wage, art }
+        : { ...items, section };
+
+    items = Object.fromEntries(
+      Object.entries(items).filter(
+        ([key, value]) => value && user[key] !== value
+      )
+    );
+
+    const { error } = (
+      type === AccountType.artist
+        ? profileArtistUpdateSchemas
+        : profileEnterpriseUpdateSchemas
+    ).validate(items);
+
+    if (error) {
+      setInputeErrorMessage(error.message);
+      return;
+    }
+
+    if (image) items["image"] = image;
+
+    try {
+      updateLoggedUser(items);
+      toogleProfileUpdateVisibility();
+    } catch (e: any) {
+      console.error(e);
+    }
+  };
 
   return (
     <Modal hidden={hideProfileUpdate}>
@@ -54,10 +95,17 @@ function ProfileUpdate() {
               />
               <h1>Atualizar perfil</h1>
             </HeaderLogo>
-            <SignContainer>
+            <SignContainer
+              onSubmit={(e: any) => {
+                e.preventDefault();
+                update();
+              }}
+            >
               <FormInputHalfField
                 type="text"
-                placeholder="Nome"
+                placeholder={`Nome ${
+                  type === AccountType.artist ? "Artístico" : "Fantasia"
+                }`}
                 value={name}
                 onChange={({ target }) => setName(target.value)}
               />
@@ -66,13 +114,21 @@ function ProfileUpdate() {
                 accept="image/*"
                 size={40000}
                 placeholder="Foto de perfil"
-                onChange={({ target }) => setImage(target.value)}
+                onChange={({ target }) => setImage(target.files?.item(0))}
+              />
+              <FormInputFullField
+                type="url"
+                placeholder="Website"
+                value={website}
+                onChange={({ target }) => setWebsite(target.value)}
               />
               {type === AccountType.artist ? (
                 <>
                   <FormSelectField
                     value={art}
-                    onChange={({ target }) => setArt(target.value)}
+                    onChange={({ target }) =>
+                      setArt(ArtTypesUtil.parse(target.value))
+                    }
                   >
                     <option disabled selected>
                       Arte
@@ -84,7 +140,7 @@ function ProfileUpdate() {
                   <FormInputFullField
                     type="number"
                     value={wage}
-                    onChange={({ target }) => setWage(parseFloat(target.value))}
+                    onChange={({ target }) => setWage(target.valueAsNumber)}
                   />
                 </>
               ) : (
